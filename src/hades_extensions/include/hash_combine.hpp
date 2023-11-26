1d73a8fb8bfe79540f399c5c0d5b6fb012fcae30
Aspect1103 <jack.ashwell1@gmail.com>
2023-11-26 14:45:56 +0000

    f

--- src/hades_extensions/include/hash_combine.hpp
+++ src/hades_extensions/include/hash_combine.hpp
@@ -8,6 +8,6 @@
 /// @param value - The value to hash.
 template <typename T>
 inline void hash_combine(size_t &seed, const T &value) {
-  std::hash<T> hasher;
+  std::hash<T> hasher ;
   seed ^= hasher(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);  // NOLINT
 }
