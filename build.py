ee572513825b951c2c662608ab0e391af63b0236
Aspect1103 <jack.ashwell1@gmail.com>
2023-11-26 21:03:38 +0000

    f

--- build.py
+++ build.py
@@ -44,7 +44,6 @@ class CMakeBuild(build_ext):
         build_dir.mkdir(parents=True, exist_ok=True)
 
         # Compile and build the CMake extension
-        print("uhf4rugh", build_dir.exists())
         subprocess.run(
             " ".join(
                 [
