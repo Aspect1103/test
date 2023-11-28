364df35d87987d358e9359d1c6f540934b8a8cbb
Aspect1103 <jack.ashwell1@gmail.com>
2023-11-28 10:47:52 +0000

    f

--- src/hades/__init__.py
+++ src/hades/__init__.py
@@ -13,8 +13,4 @@ __license__ = "MIT"
 __version__ = "0.1.0"
 
 # Test if we're running the tests on Linux in CI
-print("BUYFREGBUYRTBGRT", os.getenv("GITHUB_ACTIONS"), os.getenv("RUNNER_OS"))
-if (
-    os.getenv("GITHUB_ACTIONS") == "true" and os.getenv("RUNNER_OS") == "Linux"
-):  # pragma: no cover
-    pyglet.options["headless"] = True
+pyglet.options["headless"] = True
