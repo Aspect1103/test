255a9168ad99e4f77721cec06333d34b1921b2d2
Aspect1103 <jack.ashwell1@gmail.com>
2023-11-29 21:37:50 +0000

    f

--- src/hades_extensions/include/hash_combine.hpp
+++ src/hades_extensions/include/hash_combine.hpp
@@ -6,8 +6,10 @@
 ///
 /// @param seed - The seed for initialising the hasher.
 /// @param value - The value to hash.
+// NOLINTBEGIN
 template <typename T>
-inline void hash_combine(size_t &seed, const T &value) {
-  std::hash<T> hasher ;
-  seed ^= hasher(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);  // NOLINT
+inline void hash_combine(std::size_t &seed, const T &value) {
+  std::hash<T> hasher;
+  seed ^= hasher(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
+// NOLINTEND
