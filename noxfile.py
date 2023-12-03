7cd71e7d7bfb95e0fa71f213f4cdaeb003483150
Aspect1103 <jack.ashwell1@gmail.com>
2023-12-03 15:30:55 +0000

    f

--- noxfile.py
+++ noxfile.py
@@ -6,26 +6,10 @@ from __future__ import annotations
 import nox
 
 
-# @nox.session(python=["3.10", "3.11"])
-# def clean(session: nox.Session) -> None:
-#     """TODO."""
-#     session.install("pytest-cov")
-#     session.run("coverage", "erase")
-#     session.notify("tests")
-
-
 @nox.session(python=["3.10", "3.11"])
 def tests(session: nox.Session) -> None:
-    """TODO."""
+    """Run the pytest tests."""
     session.install(".")
     session.install("pytest-cov")
     session.run_always("python", "-m", "build", "--cpp")
     session.run("pytest", "--cov-append")
-    # session.notify("report")
-
-
-# @nox.session(python=["3.10", "3.11"])
-# def report(session: nox.Session) -> None:
-#     """TODO."""
-#     session.install("pytest-cov")
-#     session.run("coverage", "lcov")
