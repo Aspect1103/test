e75805ec66d11bfec4fccca1fcdc38c43033c029
Aspect1103 <jack.ashwell1@gmail.com>
2024-01-03 21:04:01 +0000

    fr

--- src/hades/__init__.py
+++ src/hades/__init__.py
@@ -5,6 +5,9 @@ from __future__ import annotations
 # Builtin
 import os
 
+# Pip
+import pyglet
+
 __author__ = "Aspect1103"
 __license__ = "GNU GPLv3"
 __version__ = "0.1.0"
@@ -13,4 +16,4 @@ __version__ = "0.1.0"
 if (
     os.getenv("GITHUB_ACTIONS") == "true" and os.getenv("RUNNER_OS") == "Linux"
 ):  # pragma: no cover
-    os.environ["ARCADE_HEADLESS"] = "True"
+    pyglet.options["headless"] = True  # type: ignore[misc]
